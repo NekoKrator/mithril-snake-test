d1e28ae8ac46aec376b0989c50bd1f66eb510f49
Fil Daniil <irarbc89@gmail.com>
2025-08-13 20:26:36 +0300

    refactor: restructure game into Game class with snake rendering

--- js/game.js
+++ js/game.js
@@ -1,9 +1,40 @@
-import { getCellsPerRow } from '../lib/getCellsPerRow.js';
-import { rendererBoard } from './renderer.js';
+import { getBoardConfig } from '../lib/getBoardConfig.js';
+import { rendererBoard, renderSnake, clearBoard } from './renderer.js';
+import { Snake } from './snake.js';
 
-document.querySelector('#start-btn').addEventListener('click', () => {
-  const boardSize = document.querySelector('#board-size').value;
-  const cellsPerRow = getCellsPerRow(boardSize);
+document.querySelector('#start-btn').addEventListener('click', startGame);
 
-  rendererBoard(boardSize, cellsPerRow);
-});
+function startGame() {
+  const boardName = document.querySelector('#board-size').value;
+  const boardConfig = getBoardConfig(boardName);
+
+  const game = new Game(boardConfig);
+  game.initGame();
+}
+
+class Game {
+  constructor(boardConfig) {
+    this.boardConfig = boardConfig;
+    this.snake = null;
+    this.gameLoop = null;
+    this.cells = [];
+  }
+
+  initGame() {
+    this.cells = rendererBoard(this.boardConfig.cellsPerRow);
+    this.snake = new Snake(this.boardConfig.boardCenter);
+    renderSnake(this.snake, this.cells);
+    this.gameLoop = setInterval(() => {
+      this.updateGame();
+    }, 200);
+  }
+
+  updateGame() {
+    this.snake.move();
+    this.checkCollisions();
+    clearBoard(this.cells);
+    renderSnake(this.snake, this.cells);
+  }
+
+  checkCollisions() {}
+}
