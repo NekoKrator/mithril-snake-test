90cc2bd0759410ff2833084d1eb731b599e94d40
Fil Daniil <irarbc89@gmail.com>
2025-08-13 16:46:14 +0300

    feat: add Snake class with body initialization and movement

---
+++ js/snake.js
@@ -0,0 +1,32 @@
+export class Snake {
+  constructor(boardCenter) {
+    this.direction = 'right';
+    this.body = [
+      { x: boardCenter.x, y: boardCenter.y },
+      { x: boardCenter.x - 1, y: boardCenter.y },
+      { x: boardCenter.x - 2, y: boardCenter.y },
+    ];
+  }
+
+  move() {
+    const head = this.body[0];
+    let nextHead;
+
+    switch (this.direction) {
+      case 'up':
+        nextHead = { x: head.x, y: head.y + 1 };
+        break;
+      case 'down':
+        nextHead = { x: head.x, y: head.y - 1 };
+        break;
+      case 'left':
+        nextHead = { x: head.x - 1, y: head.y };
+        break;
+      case 'right':
+        nextHead = { x: head.x + 1, y: head.y };
+    }
+
+    this.body.unshift(nextHead);
+    this.body.pop();
+  }
+}
